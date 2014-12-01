647c63c780e6d08bbf713392c486e17c4ef8e879
Richard Bellon <bellon.ricsi@gmail>
2014-12-01 17:56:52 +0100

    Újrahackelt változat

---
+++ src/ELTE.IssueR/ELTE.IssueR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELTE.IssueR.MvcApplication" Language="C#" %>
---
+++ src/ELTE.IssueR/ELTE.IssueR/Models/Account/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ELTE.IssueR.Models.Account
+{
+    public class ProfileViewModel
+    {
+
+        //TODO
+
+        public ProfileViewModel()
+        {
+            //TODO
+        }
+    }
+}
