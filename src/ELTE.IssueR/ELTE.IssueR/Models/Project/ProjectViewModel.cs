ecbac81cac364676ffe88710c5525ce78cf41a63
Bakos Peter <peter.bakos92@gmail.com>
2014-12-02 18:15:40 +0100

    Skeletons for ProjectController

---
+++ src/ELTE.IssueR/ELTE.IssueR/Models/Project/ProjectViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ELTE.IssueR.Models.Project
+{
+    public class ProjectViewModel
+    {
+    }
+}
