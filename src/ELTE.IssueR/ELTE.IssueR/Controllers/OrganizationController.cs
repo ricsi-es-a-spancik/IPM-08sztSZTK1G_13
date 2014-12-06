9018bdb3eb6352149a722638d41898bc87794149
David Hornyak <davidhornyak92@gmail.com>
2014-12-06 23:05:32 +0100

    Fancy vállalat regisztrációs form + elrejtés vendég felhasználók elől.

--- src/ELTE.IssueR/ELTE.IssueR/Controllers/OrganizationController.cs
+++ src/ELTE.IssueR/ELTE.IssueR/Controllers/OrganizationController.cs
@@ -19,19 +19,41 @@ namespace ELTE.IssueR.Controllers
         [HttpGet]
         public ActionResult Add()
         {
-            return View("Add");
+            if (Session["userName"] == null)
+            {
+                return RedirectToAction("Register", "Account");
+            }
+            else
+            {
+                return View("Add");
+            }
         }
 
         [HttpPost]
         public ActionResult Add(OrganizationViewModel orgViewModel)
         {
+            // az adatok hibás formátumban kerültek megadásra
             if (!ModelState.IsValid)
             {
-                //Nem adta meg valamelyiket.
-                ModelState.AddModelError("", "Az űrlap hibás adatokat tartlamaz.");
                 return View("Add", orgViewModel);
             }
 
+            // egyedi a vállalat neve?
+            if (_database.Organizations.Count(org => org.Name == orgViewModel.Name) != 0)
+            {
+                ModelState.AddModelError("", "A megadott néven már szerepel egy vállalat a rendszerünkben!");
+                return View("Add", orgViewModel);
+            }
+
+            // alapítás éve helyes?
+            var currentYear = System.DateTime.Today.Year;
+            if(orgViewModel.FoundationYear < 0 || orgViewModel.FoundationYear > currentYear)
+            {
+                ModelState.AddModelError("", String.Format("Az alapítás éve nem lehet kisebb, mint 0, vagy nagyobb, mint a {0}!", currentYear));
+                return View("Add", orgViewModel);
+            }
+
+            // vállalat hozzáadása az adatbázishoz
             _database.Organizations.Add(new Organization
             {
                 Name = orgViewModel.Name,
