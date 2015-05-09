a2526f9b23d836ccbbc141078fffd92453f86871
Bakos Peter <peter.bakos92@gmail.com>
2015-05-09 13:09:52 +0200

    Add Task table to db for Gantt chart

---
+++ src/ELTE.IssueR/ELTE.IssueR/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿namespace ELTE.IssueR.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class Task
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? EndDate { get; set; }
+
+        public string Resource { get; set; }
+    }
+}
